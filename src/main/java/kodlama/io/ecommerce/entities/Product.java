87f537ee5438e200521198961e7ba932c8955488
aydintas123 <aydin.220@hotmail.com>
2023-06-06 16:19:45 +0300

    sale repository added

--- src/main/java/kodlama/io/ecommerce/entities/Product.java
+++ src/main/java/kodlama/io/ecommerce/entities/Product.java
@@ -2,10 +2,13 @@ package kodlama.io.ecommerce.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Getter
@@ -18,8 +21,13 @@ public class Product  {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    @NotNull
+    @Min(0)
     private int quantity;
+    @NotNull
+    @Min(0)
     private double unitPrice;
+    @Length(min=10,max=50)
     private String description;
 
 }
